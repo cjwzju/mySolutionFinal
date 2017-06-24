5f09f0b98b0e96b9bd649f78f93c9b591fd05b39
cjwzju <1179210565@qq.com>
2017-06-24 10:21:25 +0800

    挪了一下函数。

--- src/com/cjw/solution/Solution.java
+++ src/com/cjw/solution/Solution.java
@@ -22,48 +22,5 @@ public class Solution {
 
 	}
 	
-	//完成报数的函数；
-	public static void say(int nums1[])
-	{		
-		String result=null;//输出的结果
-		boolean isSpecial=false;//是否需要报单词（即：是否特殊号码）
-		int nums[]=nums1;
-		for(int i=1;i<=100;i++)
-		{
-			result="";
-			isSpecial=false;
-			//规则（6）的优先级最高，2、3、4、5可以用字符拼接完成；
-			if((i%10-nums[0]==0)||(i-(nums[0]*10)>=0)&&(i-(nums[0]*10)<10))
-			{
-				result="Fizz";
-				isSpecial=true;
-			}
-			else
-			{
-				if(i%nums[0]==0)
-				{
-					result+="Fizz";
-					isSpecial=true;
-				}
-				if(i%nums[1]==0)
-				{
-					result+="Buzz";
-					isSpecial=true;
-				}
-				if(i%nums[2]==0)
-				{
-					result+="Whizz";
-					isSpecial=true;
-				}
-			}
-			if(!isSpecial)//非特殊号码，不报单词
-			{
-				System.out.println(i);
-			}
-			else
-			{
-				System.out.println(result);
-			}
-		}		
-	}
+	
 }
