b02d602ad228be3abf2505cca6422c3a43849245
cjwzju <1179210565@qq.com>
2017-06-24 10:12:07 +0800

    把报数 规则也单独拿出来，做一个类；

---
+++ src/com/cjw/rule/Rule.java
@@ -0,0 +1,12 @@
+package com.cjw.rule;
+
+/**
+ * 决定 把 报数规则 单独拿出来，放到一个类里面；
+ * @author chaijunwei
+ *
+ */
+public class Rule {
+	
+	
+
+}
---
+++ src/com/cjw/student/Student.java
@@ -0,0 +1,21 @@
+package com.cjw.student;
+
+/**
+ * 现在开始 使用面向对象 的 思想，把学生单独做成一个类；
+ * @author chaijunwei
+ *
+ */
+public class Student {
+
+	int Id;//学生编号；
+	
+	public Student(int id)
+	{
+		Id=id;
+	}
+	
+	public say()
+	{
+		System.out.println();
+	}
+}
